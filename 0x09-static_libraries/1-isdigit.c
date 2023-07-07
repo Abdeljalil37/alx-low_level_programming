920e8dd2aabd3a5917c3b927bbd89d988970cd42
Abdeljalil37 <widadpop@gmail.com>
2023-07-07 11:27:22 -0700

    first task has been finished

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if a character is uppercase
+ * @c: the character to check
+ *
+ * Return: 1 if the character is uppercase, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
