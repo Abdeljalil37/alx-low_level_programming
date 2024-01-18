ae10cfd07ff6e19b8ac358a1e2e1239ef09cfe06
Abdeljalil37 <widadpop@gmail.com>
2024-01-18 01:31:32 +0100

    searches for a value in a sorted array of integers using the Exponential search algorithm

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+/**
+ * interpolation_search -  searches for a value in a sorted array of
+ * integers using the Interpolation search algorithm.
+ * @array: given input array.
+ * @size: size of the given array.
+ * @value: value to search for.
+ * Return: value if exists in array otherwise -1.
+*/
+int exponential_search(int *array, size_t size, int value)
+{
+    return (-1);
+}
